0b4fd66816a3a3918c7846a0298b01acbaac48b2
Tanzim Nawaz <tntanzim@gmail.com>
2019-01-03 22:57:20 +0600

    Created labeled tokenizer

--- Assignment-3-TextClassification/XMLReader.py
+++ Assignment-3-TextClassification/XMLReader.py
@@ -1,68 +1,61 @@
 from xml.dom import minidom
 import re
-from datetime import datetime
-startTime = datetime.now()
+import nltk
+from nltk.corpus import stopwords
 
+custom_stops = []
+with open('stopwords.txt') as file:
+    for line in file:
+        custom_stops.append(line.strip('\n'))
 
-MAX_ROWS = 200
-train_file = "Dataset/Training/Anime.xml"
-test_file = "Dataset/Test/Anime.xml"
-
-TAG_RE = re.compile(r'<[^>]+>')
 
 def remove_tags(text):
-    return TAG_RE.sub('', text)
-
-# parse an xml file by name
-mydoc = minidom.parse(train_file)
-
-items = mydoc.getElementsByTagName('row')
-print('rows->>',len(items))
-index=0
-wordmap = {}
-
-count=0
-
-for item in items:
-    count=count+1
-    if count==MAX_ROWS:
-        break
-    # print('\nitem-> ' , item)
-    string = remove_tags(item.attributes['Body'].value)
-    # print('string-> ' ,string)
-    words = string.split(" ")
-    for word in words:
-        # print(word)
-        if word not in wordmap:
-            wordmap[word] = index
-            index = index + 1
-
-print(len(wordmap))
-
-vector = [0]*len(wordmap)
-
-testdoc = minidom.parse(train_file)
-
-testItems = mydoc.getElementsByTagName('row')
-
-for x in testItems:
-    string = remove_tags(x.attributes['Body'].value)
-    words = string.split(" ")
-    for w in words:
-        if w in wordmap.keys():
-            vector[wordmap[w]] = vector[wordmap[w]] + 1
-print(vector)
+    tag_re = re.compile(r'<[^>]+>')
+    return tag_re.sub('', text)
 
 
-# string = remove_tags(testItems[0].attributes['Body'].value)
-# print(string)
-# words = string.split(" ")
-# for w in words:
-#     if w in wordmap.keys():
-#         vector[wordmap[w]]=vector[wordmap[w]]+1
-#         if vector[wordmap[w]]>=2:
-#             print(w)
-#
-# print(vector)
+def tokenize(data_set, topic_list, MAX_ROWS):
+    """
 
-print(datetime.now() - startTime)
+    :param data_set: a String variable. Points to the Training or Test folder
+    :param topic_list: a list of topics to be used
+    :param MAX_ROWS: maximum number of rows of the XML file to be considered
+    :return: a list of tokenized words and labels
+    """
+    tokenized_word_list = []
+    stops = set(stopwords.words('english'))
+
+    for topic in topic_list:
+
+        file = "Dataset/" + data_set + "/" + topic + ".xml"
+        mydoc = minidom.parse(file)
+        rows = mydoc.getElementsByTagName('row')
+
+        count = 0
+        for row in rows:
+
+            if count == MAX_ROWS:
+                break
+
+            string = remove_tags(row.attributes['Body'].value).lower()
+            string = re.sub('|\n|\t|\?', '', string)
+            string = re.sub('--|/', ' ', string)
+            string = re.sub(r'http\S+|www\S+|\'|\(|\)', '', string)
+            #handle empty string
+            if not string:
+                continue
+
+            words = string.split(" ")
+
+            tokenized_line = []
+            for word in words:
+                if word != '' not in stops:
+                    if len(word) < 12 :
+                        if word not in custom_stops:
+                            tokenized_line.append(word.strip(',').strip('.'))
+
+            tokenized_word_list.append([tokenized_line,topic])
+            count += 1
+
+    return tokenized_word_list
+
